3656df23f130c9f4b2ca02e086ab93637cbf8806
Arthur Assuncao <arthur.xx92@gmail.com>
2021-08-11 22:26:30 -0300

    feat: project started

---
+++ next-seo.config.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Arthur Assuncao',
+  titleTemplate: 'Arthur Assuncao | %s',
+  defaultTitle: 'Arthur Assuncao',
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://www.arthurassuncao.com/',
+    site_name: 'Arthur Assuncao',
+  },
+};
---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from 'next/app';
+import '../assets/styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
