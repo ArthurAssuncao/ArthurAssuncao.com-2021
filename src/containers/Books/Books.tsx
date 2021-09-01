8b6c694c8257e1605c9c1482e37bd75a8dd30299
Arthur Assuncao <arthur.xx92@gmail.com>
2021-09-01 00:01:18 -0300

    feat: added description in sections

--- src/containers/Books/Books.tsx
+++ src/containers/Books/Books.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import { ButtonSection } from '../../components/ButtonSection';
+import { DescriptionSection } from '../../components/DescriptionSection';
 import { Book } from './Book';
 import styles from './Books.module.scss';
 import myBooks from './BooksData';
@@ -12,23 +13,28 @@ const Books = (props: BooksProps): JSX.Element => {
   const { className } = props;
 
   return (
-    <div className={classNames(styles.container, className)}>
+    <section className={classNames(styles.container, className)}>
       <ButtonSection title="Books" className={styles.title} />
-      <div className={styles.books}>
-        {myBooks.map((item, index) => (
-          <Book
-            key={index}
-            name={item.name}
-            description={item.description}
-            state={item.state}
-            url={item.url}
-            year={item.year}
-            publisher={item.publisher}
-            className={styles.book}
-          />
-        ))}
+      <div className={styles.content}>
+        <DescriptionSection className={styles.description}>
+          I&apos;m making learning programming easier and more accessible.
+        </DescriptionSection>
+        <div className={styles.books}>
+          {myBooks.map((item, index) => (
+            <Book
+              key={index}
+              name={item.name}
+              description={item.description}
+              state={item.state}
+              url={item.url}
+              year={item.year}
+              publisher={item.publisher}
+              className={styles.book}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/containers/MyResults/MyResults.tsx
+++ src/containers/MyResults/MyResults.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import { ButtonSection } from '../../components/ButtonSection';
+import { DescriptionSection } from '../../components/DescriptionSection';
 import { ResultsChart } from '../../components/ResultsChart';
 import styles from './MyResults.module.scss';
 
@@ -13,15 +14,20 @@ const MyResults = (props: MyResultsProps): JSX.Element => {
   return (
     <section className={classNames(className, styles.container)}>
       <ButtonSection title="My Results" className={styles.title} />
-      <ResultsChart
-        students={200}
-        projects={5}
-        yearsDeveloping={6}
-        yearsTeaching={7}
-        youtubeCourses={5}
-        youtubeLessons={40}
-        className={styles.chart}
-      />
+      <div className={styles.content}>
+        <DescriptionSection className={styles.description}>
+          Already developed projects, courses and I helped hundreds of students
+        </DescriptionSection>
+        <ResultsChart
+          students={200}
+          projects={5}
+          yearsDeveloping={6}
+          yearsTeaching={7}
+          youtubeCourses={5}
+          youtubeLessons={40}
+          className={styles.chart}
+        />
+      </div>
     </section>
   );
 };
